678b9130b082a5c978700b42b56cf04eebc2062d
Ava <ava.reesewilliams@yahoo.com>
2024-04-10 17:42:27 -0600

    adding the unversioned files? idk

---
+++ testingINTEX/Models/LineItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace testingINTEX.Models;
+
+public partial class LineItem
+{
+    public int TransactionId { get; set; }
+
+    public int ProductId { get; set; }
+
+    public int? Qty { get; set; }
+
+    public int? Rating { get; set; }
+
+    public virtual Product Product { get; set; } = null!;
+
+    public virtual Order Transaction { get; set; } = null!;
+}
