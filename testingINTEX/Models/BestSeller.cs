bd5a82f710deb276d467034dc0978743930c9674
BYU Rental <BYU Rental@RENTAL-AASGRB5>
2024-04-11 14:44:52 -0600

    idk what this is

---
+++ testingINTEX/Models/BestSeller.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace testingINTEX.Models;
+
+public partial class BestSeller
+{
+    public int BestSellersId { get; set; }
+
+    public int? ProductId { get; set; }
+
+    public int? TotalQtySold { get; set; }
+}
---
+++ testingINTEX/Models/HighlyRated.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace testingINTEX.Models;
+
+public partial class HighlyRated
+{
+    public int HighlyRatedId { get; set; }
+
+    public int? ProductId { get; set; }
+
+    public decimal? AverageRatings { get; set; }
+}
