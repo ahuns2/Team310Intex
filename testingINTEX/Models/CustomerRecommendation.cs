a30e68e42888e0a93648e665c14f54d40e7a4a9e
Ava <ava.reesewilliams@yahoo.com>
2024-04-11 13:02:23 -0600

    grant and avas work--fully updated

---
+++ testingINTEX/Models/CustomerRecommendation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace testingINTEX.Models;
+
+public partial class CustomerRecommendation
+{
+    public int CustomerId { get; set; }
+
+    public int? Recommendation1 { get; set; }
+
+    public int? Recommendation2 { get; set; }
+
+    public int? Recommendation3 { get; set; }
+
+    public int? Recommendation4 { get; set; }
+
+    public int? Recommendation5 { get; set; }
+}
